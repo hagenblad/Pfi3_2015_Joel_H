542e6b9aeabda3e54c7ca31d8511955d6754b69e
JoelHenning <joelhenninghagenblad@hotmail.com>
2015-04-30 11:10:40 +0200

    Uppgift 3 klar och redovisad

--- assignment_4/src/main/java/com/example/joelhenning/assignment_4/InfoFragment.java
+++ assignment_4/src/main/java/com/example/joelhenning/assignment_4/InfoFragment.java
@@ -7,6 +7,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 
 /**
@@ -22,7 +23,11 @@ public class InfoFragment extends DialogFragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+        return view;
     }
 
 }
